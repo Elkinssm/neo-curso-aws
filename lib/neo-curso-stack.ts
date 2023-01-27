071cc912f643e4f285379ae4c8e4e2fe0d090b00
Elkin Stiven Silva Manrique <elkin.silva@neoris.com>
2023-01-27 09:30:30 -0500

    Add archives

--- lib/neo-curso-stack.ts
+++ lib/neo-curso-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { join } from "path";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class NeoCursoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // 1) Creamos la lambda function
+    // 2) Enlazamos nuestra lambda a un construct
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NeoCursoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myLambda = new Function(this, "my-lambda-user", {
+      runtime: Runtime.NODEJS_18_X,
+      code: Code.fromAsset(join(__dirname, "../lambdas")),
+      handler: "hello.world",
+    });
+
+    //Creamos el gateway
+    const api = new RestApi(this, "usergateway", {
+      description: "apigateway for users",
+    });
+
+    //http:servidor/prjoect/user
+    const pathURL = api.root.addResource("user");
+    pathURL.addMethod("GET", new LambdaIntegration(myLambda));
+    pathURL.addMethod("POST", new LambdaIntegration(myLambda));
+    // pathURL.addMethod("PUT");
+    // pathURL.addMethod("DELETE");
   }
 }
